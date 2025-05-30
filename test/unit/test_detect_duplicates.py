f591ff207e0e1c4401d21f9e9a11f066531d19e7
Sandy Nguyen <Sandynguyen701@yahoo.se>
2025-05-30 16:47:01 +0200

    test

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,12 @@
 import pytest
+from src.util.detector import detect_duplicates
 
 # develop your test cases here
 
 @pytest.mark.unit
 def test_detect_duplicates():
     assert True
+
+def test_less_than_two_articles():
+    with pytest.raises(ValueError):
+        detect_duplicates("")
